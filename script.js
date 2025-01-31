eb6031361c6e135ab7639a73bfe8a0707a97668b
Ali Nassar <aliinassar8@gmail.com>
2025-02-01 00:12:41 +0200

    Setting Game Variables

--- script.js
+++ script.js
@@ -4,3 +4,7 @@ document.title = gameName;
 document.querySelector("h1").innerHTML = gameName;
 document.querySelector("footer").innerHTML = `${gameName} Created By Nassarov`;
 
+// Setting Game Options
+let numOfTries = 5;
+let numOfLetters = 6;
+let currentTry = 1; // initial state focus on try 1
