25a7ba49d7927554d59b5b9d1558aa0375434329
Ali Nassar <aliinassar8@gmail.com>
2025-02-01 00:11:53 +0200

    Setting Game Name

---
+++ script.js
@@ -0,0 +1,6 @@
+// Setting Game Name
+let gameName = "Guess The Word";
+document.title = gameName;
+document.querySelector("h1").innerHTML = gameName;
+document.querySelector("footer").innerHTML = `${gameName} Created By Nassarov`;
+
