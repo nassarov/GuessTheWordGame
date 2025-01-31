e42ef8e1334f1c802b3b461f406170e33079d145
Ali Nassar <aliinassar8@gmail.com>
2025-02-01 00:14:14 +0200

    Generating Inputs-disable other inputs

--- script.js
+++ script.js
@@ -8,3 +8,23 @@ document.querySelector("footer").innerHTML = `${gameName} Created By Nassarov`;
 let numOfTries = 5;
 let numOfLetters = 6;
 let currentTry = 1; // initial state focus on try 1
+
+function generateInputs() {
+  const inputsContainer = document.querySelector(".inputs");
+  for (let i = 1; i <= numOfTries; i++) {
+    const tryDiv = document.createElement("div");
+    tryDiv.classList.add(`try-${i}`);
+    tryDiv.innerHTML = `<span>Try ${i}</span>`;
+
+    // disable all except focused
+    if (i !== 1) {
+      tryDiv.classList.add("disabled-inputs");
+    }
+
+    
+
+    inputsContainer.appendChild(tryDiv);
+  }
+}
+
+window.onload = () => generateInputs();
